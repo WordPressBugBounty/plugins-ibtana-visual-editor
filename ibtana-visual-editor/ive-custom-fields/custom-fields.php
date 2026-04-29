9a43ec4546240804de48032c676cc375377725b5
Sean Murphy <sean@iamseanmurphy.com>
2026-04-29 07:05:24 +0000

    1.2.6.1

--- ibtana-visual-editor/ive-custom-fields/custom-fields.php
+++ ibtana-visual-editor/ive-custom-fields/custom-fields.php
@@ -1,8 +1,8 @@
 <?php
 // enqueue admin css for ive custom fields start //
 function enqueue_admin_styles() {
-    // wp_register_style('ive-custom-fields', plugin_dir_url(__FILE__) . 'assets/ive-cf-admin.css', array(), '1.0.0', 'all');
-    // wp_enqueue_style('ive-custom-fields');
+  wp_register_style('ive-custom-fields', plugin_dir_url(__FILE__) . 'assets/ive-cf-admin.css', array(), '1.0.1', 'all');
+  wp_enqueue_style('ive-custom-fields');
     wp_enqueue_script('ive-custom-js', plugin_dir_url(__FILE__) . 'assets/ive-admin-custom.js', array('jquery'), '1.0.0', true);
 
 }
@@ -29,10 +29,11 @@ add_action('admin_enqueue_scripts', 'enqueue_admin_styles');
         var p_this = jQuery(this);
         row_no = parseFloat(row_no);
         var row_html = jQuery('.wc-item-table .wc-hide-tr').html().replace(/rand_no/g, row_no).replace(/hide_custom_repeater_item/g, 'custom_repeater_item');
-        jQuery('.wc-item-table tbody').append('<tr class="wc-sub-row">' + row_html + '</div>');
+        jQuery('.wc-item-table tbody').append('<tr class="wc-sub-row">' + row_html + '</tr>');
       });
     });
   </script>
+  <div class="ive-fields-builder-wrap">
   <table class="wc-item-table" width="100%">
     <tbody>
       <?php
@@ -41,27 +42,27 @@ add_action('admin_enqueue_scripts', 'enqueue_admin_styles');
           ?>
           <tr class="wc-sub-row">
             <td>
-              <p><?php esc_html_e( 'Title:', 'ibtana-visual-editor' );?></p>
-              <input name="<?php echo esc_attr('custom_repeater_item['.$item_key.'][f_title]'); ?>" type="text" value="<?php echo (isset($item_value['f_title'])) ? esc_attr($item_value['f_title']) : ''; ?>" style="width:98%;" placeholder="Title" required>
+              <p class="ive-field-label"><?php esc_html_e( 'Title:', 'ibtana-visual-editor' );?></p>
+              <input class="ive-field-input" name="<?php echo esc_attr('custom_repeater_item['.$item_key.'][f_title]'); ?>" type="text" value="<?php echo (isset($item_value['f_title'])) ? esc_attr($item_value['f_title']) : ''; ?>" style="width:98%;" placeholder="Field Title" required>
             </td>
             <td>
-              <p><?php esc_html_e( 'Name:', 'ibtana-visual-editor' );?></p>
-              <input name="<?php echo esc_attr('custom_repeater_item['.$item_key.'][f_name]'); ?>" type="text" value="<?php echo (isset($item_value['f_name'])) ? esc_attr($item_value['f_name']) : ''; ?>" style="width:98%;" placeholder="Name" required>
+              <p class="ive-field-label"><?php esc_html_e( 'Name:', 'ibtana-visual-editor' );?></p>
+              <input class="ive-field-input" name="<?php echo esc_attr('custom_repeater_item['.$item_key.'][f_name]'); ?>" type="text" value="<?php echo (isset($item_value['f_name'])) ? esc_attr($item_value['f_name']) : ''; ?>" style="width:98%;" placeholder="Field Name" required>
             </td>
             <td>
-              <p><?php esc_html_e( 'Type:', 'ibtana-visual-editor' );?></p>
+              <p class="ive-field-label"><?php esc_html_e( 'Type:', 'ibtana-visual-editor' );?></p>
               <?php
               // Assuming $item_key and $item_value are already defined or fetched from somewhere
               $selected_value = isset($item_value['f_type']) ? $item_value['f_type'] : '';
               ?>
-              <select name="custom_repeater_item[<?php echo $item_key; ?>][f_type]" style="width:98%;">
+              <select class="ive-field-select" name="custom_repeater_item[<?php echo $item_key; ?>][f_type]" style="width:98%;">
                 <option value="text" <?php echo ($selected_value === 'text') ? 'selected' : ''; ?>><?php esc_html_e( 'Text', 'ibtana-visual-editor' );?></option>
                 <option value="repeater" <?php echo ($selected_value === 'repeater') ? 'selected' : ''; ?>><?php esc_html_e( 'Repeater', 'ibtana-visual-editor' );?></option>
                 <option value="checkbox" <?php echo ($selected_value === 'checkbox') ? 'selected' : ''; ?>><?php esc_html_e( 'Checkbox', 'ibtana-visual-editor' );?></option>
               </select>
             </td>
-            <td>
-              <button class="wc-remove-item button" type="button"><?php esc_html_e( 'Remove', 'ibtana-visual-editor' );?></button>
+            <td class="ive-action-cell">
+              <button class="wc-remove-item button button-secondary" type="button"><?php esc_html_e( 'Remove', 'ibtana-visual-editor' );?></button>
             </td>
           </tr>
           <?php
@@ -70,32 +71,33 @@ add_action('admin_enqueue_scripts', 'enqueue_admin_styles');
       ?>
       <tr class="wc-hide-tr" style="display: none;">
         <td>
-          <p><?php esc_html_e( 'Title:', 'ibtana-visual-editor' );?></p>
-          <input name="hide_custom_repeater_item[rand_no][f_title]" type="text" value=""  placeholder="Field Title" style="width:98%;">
+          <p class="ive-field-label"><?php esc_html_e( 'Title:', 'ibtana-visual-editor' );?></p>
+          <input class="ive-field-input" name="hide_custom_repeater_item[rand_no][f_title]" type="text" value=""  placeholder="Field Title" style="width:98%;">
         </td>
         <td>
-          <p><?php esc_html_e( 'Name:', 'ibtana-visual-editor' );?></p>
-          <input name="hide_custom_repeater_item[rand_no][f_name]" type="text" value=""  placeholder="Field Name" style="width:98%;">
+          <p class="ive-field-label"><?php esc_html_e( 'Name:', 'ibtana-visual-editor' );?></p>
+          <input class="ive-field-input" name="hide_custom_repeater_item[rand_no][f_name]" type="text" value=""  placeholder="Field Name" style="width:98%;">
         </td>
         <td>
-          <p><?php esc_html_e( 'Type:', 'ibtana-visual-editor' );?></p>
-          <select name="hide_custom_repeater_item[rand_no][f_type]" style="width:98%;">
+          <p class="ive-field-label"><?php esc_html_e( 'Type:', 'ibtana-visual-editor' );?></p>
+          <select class="ive-field-select" name="hide_custom_repeater_item[rand_no][f_type]" style="width:98%;">
             <option value="text"><?php esc_html_e( 'Text', 'ibtana-visual-editor' );?></option>
-            <option value="select"><?php esc_html_e( 'Repeater', 'ibtana-visual-editor' );?></option>
+            <option value="repeater"><?php esc_html_e( 'Repeater', 'ibtana-visual-editor' );?></option>
             <option value="checkbox"><?php esc_html_e( 'Checkbox', 'ibtana-visual-editor' );?></option>
           </select>
         </td>
-        <td>
-					<button class="wc-remove-item button" type="button"><?php esc_html_e( 'Remove', 'ibtana-visual-editor' );?></button>
+		<td class="ive-action-cell">
+					<button class="wc-remove-item button button-secondary" type="button"><?php esc_html_e( 'Remove', 'ibtana-visual-editor' );?></button>
 				</td>
 			</tr>
 		</tbody>
     <tfoot>
       <tr>
-        <td colspan="4"><button class="wc-add-item button" type="button"><?php esc_html_e( 'Add field', 'ibtana-visual-editor' );?></button></td>
+        <td colspan="4"><button class="wc-add-item button button-primary" type="button"><?php esc_html_e( 'Add field', 'ibtana-visual-editor' );?></button></td>
       </tr>
     </tfoot>
   </table>
+  </div>
   <?php
 }
 
--- ibtana-visual-editor/readme.txt
+++ ibtana-visual-editor/readme.txt
@@ -4,7 +4,7 @@ Tags: website builder, gutenberg, One click demo Import, templates, page builder
 Requires at least: 5.2
 Tested up to: 6.9
 Requires PHP: 7.2
-Stable tag: 1.2.5.9
+Stable tag: 1.2.6.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -25,7 +25,7 @@ Ibtana isn't just a plugin; it's a community! Dive into a collection of stunning
 
 **Explore the Collection of Premium Theme (Starting from $21) - [Premium WordPress Themes](https://www.vwthemes.com/collections/premium-wordpress-themes)**
 
-**Get the 250+ WordPress Theme Bundle at just $99 - [WP Theme Bundle](https://www.vwthemes.com/products/wp-theme-bundle)**
+**Get the 485+ WordPress Theme Bundle at just $99 - [WP Theme Bundle](https://www.vwthemes.com/products/wp-theme-bundle)**
 
 ## WHAT THE PLUGIN DOES?
 
@@ -144,7 +144,13 @@ Add contact form to your page with reCaptcha.
 
 == Changelog ==
 
-= 1.2.5.8 - 2026-21-03 =
+= 1.2.6.1 - 2026-29-04 =
+* **Fixes** - Minor Bug Fixes.
+
+= 1.2.6 - 2026-28-04 =
+* **Enhancement** - Custom fields ui updated.
+
+= 1.2.5.9 - 2026-21-03 =
 * **Fixes** - Security patches.
 
 = 1.2.5.8 - 2026-04-03 =
